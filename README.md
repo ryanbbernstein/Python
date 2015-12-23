8344bd31fcfe7b8203c271ccf1d74b24dfe308f2
ryanbbernstein <ryanbbernstein@gmail.com>
2015-12-22 16:27:16 -0800

    Made into class
    outputs as a list type

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Collection of python projects I've done
 - `logo.py` - Resizes an image to logo dimension 	Creates 2 resized copies of an image: One with 600 width and the other with 100 height and extents the width to 640
 
 - `demandforce_spider.py` - Scrapes all dentists in the U.S. off of local.intuit.com (DemandForce) uses Scrapy framework
+
+- `sitemap.py` - Outputs all urls in a sitemap file as a list type
--- sitemap.py
+++ sitemap.py
@@ -1,9 +1,14 @@
 # coding: utf-8
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('sitemap.xml')
-root = tree.getroot()
+class SiteMap():
+	def __init__(self):	
+		self.tree = ET.parse('sitemap.xml')
+		self.root = self.tree.getroot()
+		self.list = []
 
-for child in root:
-	url = child[0].text
-	print "\"" + url + "\","
+	def get(self):
+		for child in self.root:
+			url = child[0].text
+			self.list.append(url)
+		return self.list

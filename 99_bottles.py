1780797a1d322535327b5041ec6a2600b94f5cb5
Ryan Bernstein <ryanbbernstein@gmail.com>
2017-04-14 12:49:32 -0700

    Update to make shorter
    Added conditional in format to remove unnecessary lines.

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,4 +1,2 @@
-word = " bottles of beer"
 for x in range(99, 0, -1):
-	if x == 1: word = " bottle of beer"
-	print "{0}{1} on the wall\n{0}{1}\nTake one down, pass it around\n{2}{1} on the wall\n ".format(x, word, x - 1)
+	print "{0} bottle{1} of beer on the wall\n{0} bottle{1} of beer\nTake one down, pass it around\n{2} bottle{1} of beer on the wall\n ".format(x, "s" if x != 1 else "", x - 1)
